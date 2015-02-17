0ab4aac16d4551374846570ac5cdcfe368176cce
dylankolson <dak5332@206-L27.up.ist.local>
2015-02-16 20:11:46 -0500

    Splash Screen

--- IST311Round2/src/ist311round2/SplashPanel.java
+++ IST311Round2/src/ist311round2/SplashPanel.java
@@ -1,27 +1,43 @@
 
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.*;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author nml5182
  */
+public class SplashPanel extends JPanel {
 
-public class SplashPanel extends JPanel 
-{
     JLabel title;
-    JLabel picture;
+
+    private JLabel picture = new JLabel("TEST");
+    private ImageIcon theSplashImage;
     
-    public SplashPanel()
+  
+
+    public SplashPanel() {
+         
+     theSplashImage = new ImageIcon(this.getClass().getResource("splash.jpg"));
+
+       
+            
+
+    }
+
+    @Override
+   public void paintComponent(Graphics g)
     {
         
+        g.drawImage(theSplashImage.getImage(),0,0,null);
     }
-    
-    
 }
